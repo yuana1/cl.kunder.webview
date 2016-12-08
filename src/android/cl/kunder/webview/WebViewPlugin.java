0009abb16e9c561c51c2752ffe0c56af8e63ec96
zhaokaiyuan <zhaoky0918@gmail.com>
2016-12-08 16:01:03 +0800

    可开启多个webview

--- src/android/cl/kunder/webview/WebViewPlugin.java
+++ src/android/cl/kunder/webview/WebViewPlugin.java
@@ -13,136 +13,145 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.apache.cordova.LOG;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
 
 public class WebViewPlugin extends CordovaPlugin {
 
-  private static final String LOG_TAG = "WebViewPlugin";
-  private static CallbackContext subscribeCallbackContext = null;
-  private static CallbackContext subscribeExitCallbackContext = null;
-  private static JSONObject subscribeCallbackData = null;
+
+    private static final String LOG_TAG = "WebViewPlugin";
+    private CallbackContext subscribeCallbackContext = null;
+    private static Stack<CallbackContext> subscribeCallbackContexts = new Stack<CallbackContext>();
+    private static CallbackContext subscribeExitCallbackContext = null;
+    private static JSONObject subscribeCallbackData = null;
+    private static int webViewSize = 0;
 
 
-  public WebViewPlugin() {
-
-  }
-
-  /**
-  * Sets the context of the Command. This can then be used to do things like
-  * get file paths associated with the Activity.
-  *
-  * @param cordova
-  *            The context of the main Activity.
-  * @param webView
-  *            The CordovaWebView Cordova is running in.
-  */
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
-  }
-
-  /**
-  * Executes the request and returns PluginResult.
-  *
-  * @param action
-  *            The action to execute.
-  * @param args
-  *            JSONArry of arguments for the plugin.
-  * @param callbackContext
-  *            The callback id used when calling back into JavaScript.
-  * @return True if the action was valid, false if not.
-  */
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("show") && args.length() > 0) {
-      LOG.d(LOG_TAG, "Show Web View");
-      final String url = args.getString(0);
-      Boolean shouldShowLoading = false;
-      try{
-        shouldShowLoading = args.getBoolean(1);
-      }
-      catch(Exception e){
-
-      }
-      if(!"".equals(url)) {
-        showWebView(url, shouldShowLoading);
-        JSONObject r = new JSONObject();
-        r.put("responseCode", "ok");
-        callbackContext.success(r);
-      }
-      else {
-        callbackContext.error("Empty Parameter url");
-      }
-
-    }
-    else if(action.equals("hide")) {
-      LOG.d(LOG_TAG, "Hide Web View");
-      try {
-        subscribeCallbackData = args.getJSONObject(0);
-      } catch (Exception e) {
-        subscribeCallbackData = new JSONObject();
-      }
-      hideWebView();
-      JSONObject r = new JSONObject();
-      r.put("responseCode", "ok");
-      callbackContext.success(r);
-    }
-
-    else if(action.equals("hideLoading")) {
-      LOG.d(LOG_TAG, "Hide Web View Loading");
-      try{
-        WebViewActivity.hideLoading();
-      }
-      catch(Exception e){
-        LOG.e(LOG_TAG, "Error in hideLoading");
-        LOG.e(LOG_TAG, e.toString());
-      }
-      JSONObject r = new JSONObject();
-      r.put("responseCode", "ok");
-      callbackContext.success(r);
-    }
-
-    else if(action.equals("subscribeCallback")){
-      LOG.d(LOG_TAG, "Subscribing Cordova CallbackContext");
-      subscribeCallbackContext = callbackContext;
-    }
-
-    else if(action.equals("subscribeExitCallback")){
-      LOG.d(LOG_TAG, "Subscribing Cordova ExitCallbackContext");
-      subscribeExitCallbackContext = callbackContext;
-    }
-
-    else if(action.equals("exitApp")){
-      LOG.d(LOG_TAG, "Exiting app?");
-      if(subscribeExitCallbackContext != null){
-        subscribeExitCallbackContext.success();
-        subscribeExitCallbackContext = null;
-      }
-      this.cordova.getActivity().finish();
+    public WebViewPlugin() {
 
     }
 
-    else {
-      return false;
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
     }
 
-    return true;
-  }
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action          The action to execute.
+     * @param args            JSONArry of arguments for the plugin.
+     * @param callbackContext The callback id used when calling back into JavaScript.
+     * @return True if the action was valid, false if not.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("show") && args.length() > 0) {
+            LOG.d(LOG_TAG, "Show Web View");
+            final String url = args.getString(0);
+            Boolean shouldShowLoading = false;
+            try {
+                shouldShowLoading = args.getBoolean(1);
+            } catch (Exception e) {
 
-  private void showWebView(final String url, Boolean shouldShowLoading) {
-    LOG.d(LOG_TAG, "Url: " + url);
-    Intent i = new Intent(this.cordova.getActivity(), WebViewActivity.class);
-    i.putExtra("url", url);
-    i.putExtra("shouldShowLoading", shouldShowLoading);
-    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    this.cordova.getActivity().getApplicationContext().startActivity(i);
-  }
+            }
+            if (!"".equals(url)) {
 
-  private void hideWebView() {
-    LOG.d(LOG_TAG, "hideWebView");
-    this.cordova.getActivity().finish();
-    if(subscribeCallbackContext != null){
-      LOG.d(LOG_TAG, "Calling subscribeCallbackContext success");
-      subscribeCallbackContext.success(subscribeCallbackData);
-      subscribeCallbackContext = null;
+
+                synchronized (WebViewPlugin.class) {
+                    showWebView(url, shouldShowLoading);
+                    subscribeCallbackContexts.push(null);
+                    webViewSize++;
+                }
+                JSONObject r = new JSONObject();
+                r.put("responseCode", "ok");
+                callbackContext.success(r);
+            } else {
+                callbackContext.error("Empty Parameter url");
+            }
+
+        } else if (action.equals("hide")) {
+            LOG.d(LOG_TAG, "Hide Web View");
+            try {
+                subscribeCallbackData = args.getJSONObject(0);
+            } catch (Exception e) {
+                subscribeCallbackData = new JSONObject();
+            }
+            hideWebView();
+            JSONObject r = new JSONObject();
+            r.put("responseCode", "ok");
+            callbackContext.success(r);
+        } else if (action.equals("hideLoading")) {
+            LOG.d(LOG_TAG, "Hide Web View Loading");
+            try {
+                WebViewActivity.hideLoading();
+            } catch (Exception e) {
+                LOG.e(LOG_TAG, "Error in hideLoading");
+                LOG.e(LOG_TAG, e.toString());
+            }
+            JSONObject r = new JSONObject();
+            r.put("responseCode", "ok");
+            callbackContext.success(r);
+        } else if (action.equals("subscribeCallback")) {
+            LOG.d(LOG_TAG, "Subscribing Cordova CallbackContext");
+            synchronized (WebViewPlugin.class) {
+                if (subscribeCallbackContexts.size() == webViewSize)
+                    subscribeCallbackContexts.pop();
+                subscribeCallbackContexts.push(callbackContext);
+            }
+        } else if (action.equals("subscribeExitCallback")) {
+            LOG.d(LOG_TAG, "Subscribing Cordova ExitCallbackContext");
+            subscribeExitCallbackContext = callbackContext;
+        } else if (action.equals("exitApp")) {
+            LOG.d(LOG_TAG, "Exiting app?");
+            if (subscribeExitCallbackContext != null) {
+                subscribeExitCallbackContext.success();
+                subscribeExitCallbackContext = null;
+            }
+            this.cordova.getActivity().finish();
+
+        } else {
+            return false;
+        }
+
+        return true;
     }
-  }
+
+    private void showWebView(final String url, Boolean shouldShowLoading) {
+        LOG.d(LOG_TAG, "Url: " + url);
+        Intent i = new Intent(this.cordova.getActivity(), WebViewActivity.class);
+        i.putExtra("url", url);
+        i.putExtra("shouldShowLoading", shouldShowLoading);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.cordova.getActivity().getApplicationContext().startActivity(i);
+    }
+
+    private void hideWebView() {
+        LOG.d(LOG_TAG, "hideWebView");
+        this.cordova.getActivity().finish();
+        synchronized (WebViewPlugin.class) {
+            webViewSize--;
+            int size = subscribeCallbackContexts.size();
+            if (size != 0) {
+                subscribeCallbackContext = subscribeCallbackContexts.pop();
+                if (subscribeCallbackContext != null) {
+                    subscribeCallbackContext.success(subscribeCallbackData);
+                }
+            }
+        }
+
+//    if(subscribeCallbackContext != null){
+//      LOG.d(LOG_TAG, "Calling subscribeCallbackContext success");
+//      subscribeCallbackContext.success(subscribeCallbackData);
+//      subscribeCallbackContext = null;
+//    }
+    }
 }
